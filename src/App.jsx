804b52c0252f2fb6aba330711ff01fd863e35d45
ghayratsamiev <Ghayrat Samiev>
2024-03-27 01:56:26 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+        <ProjectSidebar></ProjectSidebar>
+    </main>
+  );
+}
+
+export default App;
