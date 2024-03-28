ff9b17e2d2a37890e0124e36488bedd854cb4feb
ghayratsamiev <Ghayrat Samiev>
2024-03-28 13:48:39 +0900

    Handling Project Deletion

--- src/App.jsx
+++ src/App.jsx
@@ -54,9 +54,19 @@ function App() {
         })
     }
 
+    function handleDeleteProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+            }
+        })
+    }
+
     const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
     console.log(projectsState);
-    let content = <SelectableProject project={selectedProject}></SelectableProject>;
+    let content = <SelectableProject project={selectedProject} onDelete = {handleDeleteProject}></SelectableProject>;
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}></NewProject>
     } else if (projectsState.selectedProjectId === undefined) {
